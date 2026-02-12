00d9956a02035e721dd2df6fe1c88a23d1eedd72
Mahesh <mahesh@example.com>
2026-02-12 15:36:59 +0530

    Move deps to prod and rename css to bust cache

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,20 +7,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        'neo-bg': '#FFFBF0', 
+        'neo-bg': '#FFFBF0',
         'neo-black': '#121212',
         'neo-white': '#FFFFFF',
-        'neo-green': '#A3E635', 
-        'neo-blue': '#60A5FA', 
-        'neo-pink': '#F472B6', 
-        'neo-yellow': '#FACC15', 
+        'neo-green': '#A3E635',
+        'neo-blue': '#60A5FA',
+        'neo-pink': '#F472B6',
+        'neo-yellow': '#FACC15',
+      },
       fontFamily: {
         sans: ['"Space Grotesk"', 'sans-serif'],
         mono: ['"JetBrains Mono"', 'monospace'],
       },
       boxShadow: {
-        'neo': '4px 4px 0px 0px #121212', 
-        'neo-hover': '2px 2px 0px 0px #121212', 
+        'neo': '4px 4px 0px 0px #121212',
+        'neo-hover': '2px 2px 0px 0px #121212',
         'neo-lg': '8px 8px 0px 0px #121212',
       }
     },
